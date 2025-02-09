816130a9865dac0600b9eae10c9bc480d23c12e6
Alexis Praga <alexis@praga.dev>
2025-02-09 15:21:35 +0100

    Flake: several package fail on unstable

--- flake.nix
+++ flake.nix
@@ -1,21 +1,51 @@
 {
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
 
-    description = "Reproducible sarek with Nix and datalad";
+  description = "Reproducible sarek with Nix and datalad";
 
-    outputs = { self, nixpkgs }:
-        let
-            system = "x86_64-linux";
-            # Unfree is needed for VEP dendencies (kent)
-            pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-        in {
-            packages.${system} = {
-                # default = pkgs.callPackage pkgs/nextflow.nix {};
-                default = pkgs.nextflow;
-                # This install the necessary executables for all nodes
-            };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      # Unfree is needed for VEP dendencies (kent)
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          # permittedInsecurePackages = [
+            # For manta
+            # "python-2.7.18.8"
+          # ];
+          allowUnfree = true;
         };
+      };
+
+    in {
+      packages.${system} = {
+        default = pkgs.nextflow;
+        bwa = pkgs.bwa;
+        # FIXME
+        bwa-mem2 = pkgs.bwa-mem2;
+        # FIXME
+        dragmap = pkgs.dragmap;
+        samtools = pkgs.samtools;
+        # FIXME python 2
+        cnvkit = pkgs.pythonPackages.cnvkit;
+        # TODO
+        # deepVariant = pkgs.deepvariant;
+        freebayes = pkgs.freebayes;
+        # FIXME doest not work even with permittedinsecure packages
+        manta = pkgs.manta;
+        # FIXME python 2
+        strelka2 = pkgs.strelka2;
+        tiddit = pkgs.tiddit;
+        snpeff = pkgs.snpeff;
+        # wait for PR
+        # vep = pkgs.vep;
+        bcftools = pkgs.bcftools;
+        multiqc = pkgs.multiqc;
+        # This install the necessary executables for all nodes
+      };
+    };
 }
